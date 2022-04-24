f13781b3e6bfe406a974cc46b930ae325b82d24b
Julia Mazur <mazurka0609@gmail.com>
2022-04-24 12:51:53 +0200

    finally faster

--- Algorytm.py
+++ Algorytm.py
@@ -1,50 +1,63 @@
 #!/usr/bin/env python 
 
 import numpy as np
-import math
+from numba import njit
 import random
 
-def alg_met(L,T,K, file_name):
-    b = [i for i in range(-1,L+1)]
+@njit
+def alg_met(L,T,K):
+    b = np.arange(-1,L+1)
     b[0] = L-1
     b[L+1] = 0
-    kb = 1 #stała boltzmana
-    J = 1
     N=L**2
     S = np.ones((L,L))
     #S = np.random.choice([-1,1],(L,L))
-    #np.savetxt(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\L100_T2\\'+file_name+str(name_count)+'.txt',S)
-    S_data = [np.copy(S)]
-    #m_data = [1/N*sum(sum(S))]
-    for z in range(K):
+    #S_data = [np.copy(S)]
+    m_data = [S.mean()]
+    K_count = 0
+    while K_count<K:
+        K_count += 1
         op_count=0
         while op_count<N:
             op_count += 1
-            i = math.floor(random.random()*L)
-            j = math.floor(random.random()*L)
-            dE=2*J*S[i][j]*(S[b[i]][j] + S[b[i+2]][j] + S[i][b[j]] + S[i][b[j+2]])
+            i = np.random.randint(L)
+            j = np.random.randint(L)
+            dE=2*S[i][j]*(S[b[i]][j] + S[b[i+2]][j] + S[i][b[j]] + S[i][b[j+2]])
             if dE<=0:
                 S[i][j] *= -1
             else:
                 x = random.random()
-                if x<math.exp(-dE/(kb*T)):
+                if x<np.exp(-dE/T):
                     S[i][j] *= -1
-        #m_data.append(1/N*sum(sum(S)))
-        S_data.append(np.copy(S))
-    np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\L100_T2\\'+file_name,S_data, allow_pickle=True)
-    #np.savetxt(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\danem\\'+file_name+'m.txt',m_data)
+        m_data.append(S.mean())
+        #S_data.append(np.copy(S))
+    #return S_data
+    return m_data
 
     
 if __name__ == "__main__":
-    #alg_met(10,2.26,10**6,"L10_T1")
-    #alg_met(10,2.26,10**6,"L10_T2_")
-    #alg_met(10,4,10**6,"L10_T3_")
-    #alg_met(100,2.26,10**6,"L100_T2")
-    #alg_met(10,1.7,10**6,"uno_1_7_10")
-    #alg_met(10,1.7,10**6,"1_7_10")
-    #alg_met(10,1.7,10**6,"1_7_10_3")
-    #alg_met(50,1.7,10**6,"1_7_50")
-    alg_met(100,2.26,10**6,"L100_T2")
+    #s1_1 = alg_met(10,1,10**6)
+    #np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\dane\\'+"L10_T1",s1_1, allow_pickle=True)
+    #s1_2 = alg_met(10,2.26,10**6)
+    #np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\dane\\'+"L10_T2",s1_2, allow_pickle=True)
+    #s1_3 = alg_met(10,4,10**6)
+    #np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\dane\\'+"L10_T3",s1_3, allow_pickle=True)
+    #s2_1 = alg_met(100,1,10**4)
+    #np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\dane\\'+"L100_T1",s2_1, allow_pickle=True)
+    #s2_2 = alg_met(100,2.26,10**4)
+    #np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\dane\\'+"L100_T2",s2_2, allow_pickle=True)
+    #s2_3 = alg_met(100,4,10**5)
+    #np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\dane\\'+"L100_T3",s2_3, allow_pickle=True)
+    #m10 = alg_met(10,1.7,10**6)
+    #np.savetxt(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\danem\\m10.txt',m10)
+    #m50 = alg_met(10,1.7,10**6)
+    #np.savetxt(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\danem\\m50.txt',m50)
+    #m100 = alg_met(10,1.7,10**6)
+    #np.savetxt(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\danem\\m100.txt',m100)
+    m=[]
+    for i in np.arange(1,3.5,0.2):
+        m.append(alg_met(10,i,10**6))
+    np.save(r'C:\\Users\\mazur\\OneDrive\\Dokumenty\\GitHub\\Fizyka-ukladow-zlozonych\\danem\\'+"m_zakres",m, allow_pickle=True)
 
 
     # alg_met(10,1,10**6,"L10_T1_2_")
